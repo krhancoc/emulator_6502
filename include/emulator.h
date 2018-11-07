9c75d31dcbff70b1eff48579dfff672b8b3718f2
Haoyu Gu <guhaoyu2005@gmail.com>
2018-11-07 00:05:50 -0500

    Quick syntax fix

--- include/emulator.h
+++ include/emulator.h
@@ -89,7 +89,7 @@ class Emulator {
     // to set flag bit, do OR
     const word p_bit[8] = {
 	    0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80
-    }
+    };
 
     vector<Instruction *> program;
     address current_inst  = 0;
